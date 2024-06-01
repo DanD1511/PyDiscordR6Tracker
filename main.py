8754200bd3f9f4a4e1a8a0f5b91a18c3eed5ed97
Dan <ddacunha012001@gmail.com>
2024-06-01 15:29:55 -0500

    gitignore added

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from siegeapi import Auth
+import asyncio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+async def sample():
+    auth = Auth('danilod0129@outlook.com', 'Eyleen0129#')
+    player = await auth.get_player(name="Microsoft.365")
 
+    print(f"Name: {player.name}")
+    print(f"Profile pic URL: {player.profile_pic_url}")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    await player.load_persona()
+    print(f"Streamer nickname: {player.persona.nickname}")
+    print(f"Nickname enabled: {player.persona.enabled}")
 
+    await player.load_playtime()
+    print(f"Total Time Played: {player.total_time_played:,} seconds / {player.total_time_played_hours:,} hours")
+    print(f"Level: {player.level}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    await player.load_ranked_v2()
+    print(f"Ranked Points: {player.ranked_profile.rank_points}")
+    print(f"Rank: {player.ranked_profile.rank}")
+    print(f"Max Rank Points: {player.ranked_profile.max_rank_points}")
+    print(f"Max Rank: {player.ranked_profile.max_rank}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    await player.load_progress()
+    print(f"XP: {player.xp:,}")
+    print(f"Total XP: {player.total_xp:,}")
+    print(f"XP to level up: {player.xp_to_level_up:,}")
+
+    await auth.close()
+
+asyncio.run(sample())
